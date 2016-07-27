8f14dce50b618bdff166c862684228a0ba2c9805
Benja Rabenasolo <rabenjasolo@gmail.com>
2016-07-27 20:33:59 +0300

    Adding deleteUser.php file

---
+++ ajax/deleteUser.php
@@ -0,0 +1,15 @@
+<?php
+// Check request
+if (isset($_POST['id']) && isset($_POST['id']) != "") {
+    // include Database connection file
+    include("../db/dbConnexion.php") ;
+
+    // get user id
+    $user_id = $_POST['id'] ;
+
+    // delete User
+    $query = "DELETE FROM users WHERE id = '$user_id'" ;
+    if (!$result = mysqli_query($con, $query)) {
+        exit(mysqli_error($con)) ;
+    }
+}
